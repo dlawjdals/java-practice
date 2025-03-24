c250cdc31428ae1d5ab0f7cff66d1da185ba66e2
dlawjdals <yjm91804640@gmail.com>
2025-03-24 15:54:47 +0900

    4주차 원의 면적 넓이 구하는 프로그램 제작 완료

---
+++ HelloWorld/src/Main.java
@@ -0,0 +1,9 @@
+
+public class Main {
+    public static void main(String[] args) {
+        double round = 10.0;
+        final double PI = 3.14;
+        double result = round * round * PI;
+        System.out.println(result);
+    }
+}
