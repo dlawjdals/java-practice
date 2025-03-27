d190f4abe5c4bf95a238994524901822fea5dfd9
dlawjdals <yjm91804640@gmail.com>
2025-03-27 14:41:45 +0900

    25.03.27 실습문제 2번

---
+++ java_choboja/src/Main.java
@@ -0,0 +1,16 @@
+
+public class Main {
+    public static void main(String[] args) {
+        // 주어진 값들
+        double doubleValue = 2.71828;
+
+        // double에서 다른 타임으로의 형변환 결과 출력
+        System.out.println("\n=== double 형변환 ===");
+        System.out.println("1. double -> byte: " + (byte)doubleValue);
+        System.out.println("1. double -> short: " + (short)doubleValue);
+        System.out.println("1. double -> int: " + (int)doubleValue);
+        System.out.println("1. double -> long: " + (long)doubleValue);
+        System.out.println("1. double -> float: " + (float)doubleValue);
+
+    }
+}
